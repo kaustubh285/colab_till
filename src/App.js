c438fac850ac6474ca469b9136cc27974195d6b9
kaustubh285 <kaustubh285@gmail.com>
2024-09-06 10:28:41 +0100

    uncommented unauth

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
-// import Unauth from "./page/Unauth";
+import Unauth from "./page/Unauth";
 import Menu from "./page/Menu";
 import Dashboard from "./page/Dashboard";
 
@@ -12,7 +12,7 @@ function App() {
     <div className='app'>
       <BrowserRouter>
         <Routes>
-          {/* <Route path='/' element={<Unauth />} /> */}
+          <Route path='/' element={<Unauth />} />
           <Route path='/menu/*' element={<Menu />} />
           <Route path='/dashboard/' element={<Dashboard />} />
         </Routes>
